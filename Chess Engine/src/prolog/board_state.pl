1bf59d3dd0b430efc9dc0b075c500b0841de5706
Chandler Silk <166081591+cs2673@users.noreply.github.com>
2025-12-05 19:29:03 -0700

    Update board_state.pl

--- Chess Engine/src/prolog/board_state.pl
+++ Chess Engine/src/prolog/board_state.pl
@@ -7,27 +7,26 @@
 % Row/Col: 1-8
 
 % Initial board setup
-initial_board
-([
+initial_board([
     % White pieces
-    piece(rook, white, 1, 1), piece(knight, white, 1, 2),
-    piece(bishop, white, 1, 3), piece(queen, white, 1, 4),
-    piece(king, white, 1, 5), piece(bishop, white, 1, 6),
-    piece(knight, white, 1, 7), piece(rook, white, 1, 8),
-    piece(pawn, white, 2, 1), piece(pawn, white, 2, 2),
-    piece(pawn, white, 2, 3), piece(pawn, white, 2, 4),
-    piece(pawn, white, 2, 5), piece(pawn, white, 2, 6),
-    piece(pawn, white, 2, 7), piece(pawn, white, 2, 8),
-    
+    piece(rook,  white, 1, 1), piece(knight, white, 1, 2),
+    piece(bishop,white, 1, 3), piece(queen,  white, 1, 4),
+    piece(king,  white, 1, 5), piece(bishop, white, 1, 6),
+    piece(knight,white, 1, 7), piece(rook,   white, 1, 8),
+    piece(pawn,  white, 2, 1), piece(pawn,   white, 2, 2),
+    piece(pawn,  white, 2, 3), piece(pawn,   white, 2, 4),
+    piece(pawn,  white, 2, 5), piece(pawn,   white, 2, 6),
+    piece(pawn,  white, 2, 7), piece(pawn,   white, 2, 8),
+
     % Black pieces
-    piece(rook, black, 8, 1), piece(knight, black, 8, 2),
-    piece(bishop, black, 8, 3), piece(queen, black, 8, 4),
-    piece(king, black, 8, 5), piece(bishop, black, 8, 6),
-    piece(knight, black, 8, 7), piece(rook, black, 8, 8),
-    piece(pawn, black, 7, 1), piece(pawn, black, 7, 2),
-    piece(pawn, black, 7, 3), piece(pawn, black, 7, 4),
-    piece(pawn, black, 7, 5), piece(pawn, black, 7, 6),
-    piece(pawn, black, 7, 7), piece(pawn, black, 7, 8)
+    piece(rook,  black, 8, 1), piece(knight, black, 8, 2),
+    piece(bishop,black, 8, 3), piece(queen,  black, 8, 4),
+    piece(king,  black, 8, 5), piece(bishop, black, 8, 6),
+    piece(knight,black, 8, 7), piece(rook,   black, 8, 8),
+    piece(pawn,  black, 7, 1), piece(pawn,   black, 7, 2),
+    piece(pawn,  black, 7, 3), piece(pawn,   black, 7, 4),
+    piece(pawn,  black, 7, 5), piece(pawn,   black, 7, 6),
+    piece(pawn,  black, 7, 7), piece(pawn,   black, 7, 8)
 ]).
 
 % Check if a square is on the board
@@ -36,9 +35,8 @@ on_board(Row, Col) :-
     Col >= 1, Col =< 8.
 
 % Find a piece at a specific position
-piece_at(Board, Row, Col, Piece) :-
-    member(piece(Type, Color, Row, Col), Board),
-    Piece = piece(Type, Color, Row, Col).
+piece_at(Board, Row, Col, piece(Type, Color, Row, Col)) :-
+    member(piece(Type, Color, Row, Col), Board).
 
 % Check if a square is empty
 is_empty(Board, Row, Col) :-
